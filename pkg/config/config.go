0d9a99e03bbf25f4de586312203d37bbcf864bb3
Nikita Kadatsky <83119704+Hikitak@users.noreply.github.com>
2025-06-19 14:21:17 +0000

    Config parsing added

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,12 +8,25 @@ import (
 )
 
 type Config struct {
-	Figma   FigmaConfig `yaml:"figma"`
+	Figma    FigmaConfig `yaml:"figma"`
+	Schedule string      `yaml:"schedule"`
+	Email    EmailConfig `yaml:"email"`
 }
 
 type FigmaConfig struct {
 	Token    string   `yaml:"token"`
 	FileKeys []string `yaml:"file_keys"`
+}
+
+type EmailConfig struct {
+	SMTPHost     string   `yaml:"smtp_host"`
+	SMTPPort     int      `yaml:"smtp_port"`
+	SMTPUsername string   `yaml:"smtp_username"`
+	SMTPPassword string   `yaml:"smtp_password"`
+	From         string   `yaml:"from"`
+	To           []string `yaml:"to"`
+	Subject      string   `yaml:"subject"`
+	Body         string   `yaml:"body"`
 }
 
 func Load(path string) (*Config, error) {
